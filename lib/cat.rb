2c565e5573e10222eb24e37de122d92bf8e9af13
Aaron Samuel <aasamuelj@gmail.com>
2017-10-03 14:43:59 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,7 +1,6 @@
 class Cat
-  # code goes here
-  attr_accessor :mood
   attr_reader :name
+  attr_accessor :mood
   def initialize(name)
     @name = name
     @mood = "nervous"
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,11 @@
 require 'pry'
-
 class Owner
-  attr_accessor :name, :pets
-  # code goes here
   @@all = []
-
+  attr_accessor :name, :pets
   def initialize(name)
     @name = name
-    self.class.all << self
-    @pets =  {fishes: [], dogs: [], cats: []}
+    @@all << self
+    @pets = {fishes: [], dogs: [], cats: []}
   end
   def self.all
     @@all
@@ -17,42 +14,38 @@ class Owner
     self.all.clear
   end
   def self.count
-    self.all.size
+    self.all.count
   end
   def species
-    species = "human"
-    species
+    "human"
   end
   def say_species
-    say_species = "I am a human."
-    say_species
+    "I am a human."
   end
-
-  def buy_fish(fish_name)
-    fish = Fish.new(fish_name)
+  def buy_fish(name)
+    fish = Fish.new(name)
     @pets[:fishes] << fish
-
   end
-  def buy_cat(cat_name)
-    cat = Cat.new(cat_name)
+  def buy_cat(name)
+    cat = Cat.new(name)
     @pets[:cats] << cat
   end
-  def buy_dog(dog_name)
-    dog = Dog.new(dog_name)
+  def buy_dog(name)
+    dog = Dog.new(name)
     @pets[:dogs] << dog
   end
   def walk_dogs
-    @pets[:dogs].each do |dog|
+    @pets[:dogs].collect do |dog|
       dog.mood = "happy"
     end
   end
   def play_with_cats
-    @pets[:cats].each do |cat|
+    @pets[:cats].collect do |cat|
       cat.mood = "happy"
     end
   end
   def feed_fish
-    @pets[:fishes].each do |fish|
+    @pets[:fishes].collect do |fish|
       fish.mood = "happy"
     end
   end
@@ -60,24 +53,11 @@ class Owner
     @pets.each do |type, pets|
       pets.each do |pet|
         pet.mood = "nervous"
-
       end
     end
     @pets.clear
   end
   def list_pets
-    array = []
-    @pets.each do |type, pets|
-      if type.to_s == "fishes"
-        array << "#{pets.size} #{type.to_s.chomp("es")}"
-      end
-      if type.to_s == "dogs"
-        array << "#{pets.size} #{type.to_s.chomp("s")}(s)"
-      end
-      if type.to_s == "cats"
-        array << "#{pets.size} #{type.to_s.chomp("s")}(s)"
-      end
-    end
-    "I have #{array[0, array.length - 1].join(', ')}, and #{array.last}."
+    
   end
-end
+
